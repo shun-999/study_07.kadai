454fd3c0cf117593f9680b44a5731cce997b57b4
shun-999 <71618433+shun-999@users.noreply.github.com>
2021-01-12 22:28:29 +0900

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,14 @@
+from googletrans import Translator
+import eel
+import desktop
+
+app_name="web"
+end_point="index3.html"
+
+@eel.expose
+def translate(word, input_lang, output_lang):   
+    tr = Translator()
+    text = tr.translate(text=word, src=input_lang, dest=output_lang).text
+    eel.translate_result(text)
+
+desktop.start(app_name, end_point)
